408ec88ab033edcc72e15a83858ddb93cf2194a6
DevSalma <devsalma6@gmail.com>
2023-05-03 09:31:27 +0100

    added async function

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
-let endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=2C3MZ44TkRpaiEIbmgNWiFvbllCEWYTh&q=dog&limit=25&offset=0&rating=g&lang=en';
+const fetch = require('isomorphic-fetch');
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+
+async function getImages(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+    const response = await fetch(endpoint);
+    console.log(endpoint);
+    const {data} = await response.json();
+    const imageUrl = data[0].url;
+    console.log(imageUrl);
+    return imageUrl;
+}
+
+ async function main(){
+    getImages(`dog`);
+ }
+
+ main();
